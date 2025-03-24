759ff91347c0cfaa5817a02116e3df7738cfe364
JoJo <lokijolijo@gmail.com>
2025-03-24 10:47:53 +0100

    replacing more dnf5 by rpm-ostree and adding an experimental recipe

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -6,7 +6,7 @@ echo "Installing Nvidia drivers"
 
 RELEASE="$(rpm -E %fedora)"
 
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
+rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 
 KERNEL_FLAVOR=main
 KERNEL_SUFFIX=""
@@ -16,6 +16,6 @@ skopeo copy docker://ghcr.io/joviatrix/akmods-nvidia-open:"${KERNEL_FLAVOR}"-"${
 AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
 tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
 
-dnf5 install -y xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda /tmp/rpms/kmods/*kmod-nvidia*.rpm /tmp/rpms/ublue-os/*ublue-os-nvidia-addons*.rpm
+rpm-ostree install xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda /tmp/rpms/kmods/*kmod-nvidia*.rpm /tmp/rpms/ublue-os/*ublue-os-nvidia-addons*.rpm
 
-dnf5 remove -y rpmfusion-free-release rpmfusion-nonfree-release
+rpm-ostree uninstall rpmfusion-free-release rpmfusion-nonfree-release
