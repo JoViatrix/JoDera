a8a98e444041ee1eac2959e3a9cd69598044bfd1
JoJo <lokijolijo@gmail.com>
2025-04-15 20:05:34 +0200

    fixing missing package

--- files/scripts/common.sh
+++ files/scripts/common.sh
@@ -8,8 +8,6 @@ RELEASE="$(rpm -E %fedora)"
 
 rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$RELEASE.noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$RELEASE.noarch.rpm
 
-rpm-ostree install zerotier-one
-
 dnf5 group install -y multimedia
 
 rpm-ostree uninstall rpmfusion-free-release rpmfusion-nonfree-release
