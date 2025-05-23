5ccac6f3f5a06f16d0907c4a103071d1713dc138
JoJo <lokijolijo@gmail.com>
2025-05-23 10:12:50 +0200

    Adding scripts to download the latest version of packages

---
+++ files/scripts/bitwarden.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+echo "Installing bitwarden"
+
+curl -L -o "/tmp/bitwarden.rpm" 'https://vault.bitwarden.com/download/?app=desktop&platform=linux&variant=rpm'
+
+
+mkdir -p /var/opt
+rpm-ostree install /tmp/bitwarden.rpm
+mv /var/opt/Bitwarden /usr/lib/opt/Bitwarden
+
+cat >/usr/lib/tmpfiles.d/bitwarden.conf <<EOF
+L /opt/Bitwarden - - - - /usr/lib/opt/Bitwarden
+EOF
---
+++ files/scripts/heroic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+echo "Installing heroic"
+
+REPO="Heroic-Games-Launcher/HeroicGamesLauncher"
+ASSET_SUFFIX="linux-x86_64.rpm"
+RELEASE_DATA=$(curl -s "https://api.github.com/repos/$REPO/releases/latest")
+ASSET_NAME=$(echo "$RELEASE_DATA" | jq -r ".assets[] | select(.name | endswith(\"$ASSET_SUFFIX\")) | .name")
+TAG=$(echo "$RELEASE_DATA" | jq -r '.tag_name')
+URL="https://github.com/$REPO/releases/download/$TAG/$ASSET_NAME"
+curl -L -o "/tmp/heroic.rpm" "$URL"
+
+
+mkdir -p /var/opt
+rpm-ostree install /tmp/heroic.rpm
+mv /var/opt/Heroic /usr/lib/opt/Heroic
+
+cat >/usr/lib/tmpfiles.d/heroic.conf <<EOF
+L /opt/Heroic - - - - /usr/lib/opt/Heroic
+EOF
