c18dd2a1210d1b0ba35bf6421b094a693b3b0bf6
JoJo <lokijolijo@gmail.com>
2025-06-04 13:51:41 +0200

    adding aaru

---
+++ files/scripts/aaru.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+echo "Installing aaru"
+
+REPO="aaru-dps/Aaru"
+ASSET_SUFFIX=".el.x86_64.rpm"
+RELEASE_DATA=$(curl -s "https://api.github.com/repos/$REPO/releases" | jq -r '[.[] | select(.prerelease == true)][0]')
+ASSET_NAME=$(echo "$RELEASE_DATA" | jq -r ".assets[] | select(.name | endswith(\"$ASSET_SUFFIX\")) | .name")
+TAG=$(echo "$RELEASE_DATA" | jq -r '.tag_name')
+URL="https://github.com/$REPO/releases/download/$TAG/$ASSET_NAME"
+curl -L -o "/tmp/aaru.rpm" "$URL"
+
+rpm-ostree install /tmp/aaru.rpm
