b51a5ede8136b630268072d9ccdb47b3b27f926a
JoJo <62498297+JoViatrix@users.noreply.github.com>
2026-02-08 12:30:42 +0100

    fixing xone firmware

--- files/scripts/xone.sh
+++ files/scripts/xone.sh
@@ -4,13 +4,15 @@ set -ouex pipefail
 
 echo "Installing xone"
 
-RELEASE="$(rpm -E %fedora)"
-KERNEL_FLAVOR=main
-KERNEL_SUFFIX=""
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+ln -s /lib/firmware/xow_dongle.bin /lib/firmware/xone_dongle_02fe.bin
 
-skopeo copy docker://ghcr.io/joviatrix/akmods:"${KERNEL_FLAVOR}"-"${RELEASE}"-"${QUALIFIED_KERNEL}" dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
-tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+# RELEASE="$(rpm -E %fedora)"
+# KERNEL_FLAVOR=main
+# KERNEL_SUFFIX=""
+# QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 
-dnf install -y xone-kmod xone-kmod-common /tmp/rpms/kmods/*xone*.rpm
+# skopeo copy docker://ghcr.io/joviatrix/akmods:"${KERNEL_FLAVOR}"-"${RELEASE}"-"${QUALIFIED_KERNEL}" dir:/tmp/akmods
+# AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
+# tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+
+# dnf install -y xone-kmod xone-kmod-common /tmp/rpms/kmods/*xone*.rpm
