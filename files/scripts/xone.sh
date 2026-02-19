b0d6f105be370268bf55f41c07c637938fca6dd3
JoJo <62498297+JoViatrix@users.noreply.github.com>
2026-02-19 19:33:40 +0100

    reverting back to kinoite

--- files/scripts/xone.sh
+++ files/scripts/xone.sh
@@ -6,13 +6,13 @@ echo "Installing xone"
 
 ln -s /lib/firmware/xow_dongle.bin /lib/firmware/xone_dongle_02fe.bin
 
-# RELEASE="$(rpm -E %fedora)"
-# KERNEL_FLAVOR=main
-# KERNEL_SUFFIX=""
-# QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+RELEASE="$(rpm -E %fedora)"
+KERNEL_FLAVOR=main
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 
-# skopeo copy docker://ghcr.io/joviatrix/akmods:"${KERNEL_FLAVOR}"-"${RELEASE}"-"${QUALIFIED_KERNEL}" dir:/tmp/akmods
-# AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
-# tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+skopeo copy docker://ghcr.io/joviatrix/akmods:"${KERNEL_FLAVOR}"-"${RELEASE}"-"${QUALIFIED_KERNEL}" dir:/tmp/akmods
+AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
 
-# dnf install -y xone-kmod xone-kmod-common /tmp/rpms/kmods/*xone*.rpm
+dnf install -y xone-kmod xone-kmod-common /tmp/rpms/kmods/*xone*.rpm
