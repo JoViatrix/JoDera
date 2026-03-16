f1b88e62815c11de8da6758d841398cb4ac4f6a2
JoViatrix <62498297+JoViatrix@users.noreply.github.com>
2026-03-16 09:33:05 +0000

    Update aaru.sh

--- files/scripts/aaru.sh
+++ files/scripts/aaru.sh
@@ -5,13 +5,11 @@ set -ouex pipefail
 echo "Installing aaru"
 
 REPO="aaru-dps/Aaru"
-ASSET_SUFFIX="_linux_amd64.tar.xz"
+ASSET_SUFFIX=".x86_64.rpm"
 RELEASE_DATA=$(curl -s "https://api.github.com/repos/$REPO/releases" | jq -r '[.[] | select(.prerelease == true)][0]')
 ASSET_NAME=$(echo "$RELEASE_DATA" | jq -r ".assets[] | select(.name | endswith(\"$ASSET_SUFFIX\")) | .name")
 TAG=$(echo "$RELEASE_DATA" | jq -r '.tag_name')
 URL="https://github.com/$REPO/releases/download/$TAG/$ASSET_NAME"
-curl -L -o "/tmp/aaru.tar.xz" "$URL"
+curl -L -o "/tmp/aaru.rpm" "$URL"
 
-tar -xf "/tmp/aaru.tar.xz" -C /tmp/
-
-mv --update=none-fail /tmp/aaru /usr/bin/
+dnf install -y install /tmp/aaru.rpm
