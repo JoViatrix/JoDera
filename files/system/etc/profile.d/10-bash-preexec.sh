7212c3b06c22d069dcb8274ef14ec9fbc9be6542
JoJo <62498297+JoViatrix@users.noreply.github.com>
2026-09-01 11:00:54 +0200

    fix mistake

--- files/system/etc/profile.d/10-bash-preexec.sh
+++ files/system/etc/profile.d/10-bash-preexec.sh
@@ -1,4 +1,4 @@
 # Source bash-preexec before atuin
 if [[ -n "${BASH_VERSION:-}" ]]; then
-    source /usr/local/share/bash-preexec/bash-preexec.sh
+    source /usr/share/bash-preexec/bash-preexec.sh
 fi
